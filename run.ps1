b07719704d729e1a0e8574b32e03f2d38600132e
redbelter <jupiterrun@proton.me>
2025-02-15 22:54:34 -0800

    initial code

---
+++ run.ps1
@@ -0,0 +1,1 @@
+python.exe sonar.py
---
+++ sonar.py
@@ -0,0 +1,50 @@
+from steelseries_sonar_py import Sonar
+import sys
+import pywinusb.hid as hid
+import datetime
+import time
+
+
+def setsonarstuff():
+    print("start")
+    sonar = Sonar()
+    volume_data = sonar.get_volume_data()
+    print("Volume Data:", volume_data)
+    time.sleep(10)
+    print("sleep done")
+    sonar.set_streamer_mode(True)
+    sonar.set_streamer_mode(False)
+    volume_data = sonar.get_volume_data()
+    print("Volume Data:", volume_data)
+    print("stop")
+
+
+def watchForChanges():
+    all_hids = hid.find_all_hid_devices()
+    found = False
+    prevResult = False
+    while True:
+        found = False
+        for device in all_hids:
+            # Extract relevant information from the device and add it to the devices list
+            #Change this to whatever device you want to trigger it on
+            if(device.product_name == "HyperX QuadCast S"):
+                #print("found it")
+                if(device.is_plugged()):
+                    found = True
+                else:
+                    found = False
+        
+        if(found and not prevResult):
+            prevResult = True
+            print("it came back")
+            setsonarstuff()
+        if(not found and prevResult):
+            prevResult = False
+            print("its gone")
+
+        #print(str(datetime.datetime.now())+" found: " + str(found) + " and prevResult: " + str(prevResult))
+        time.sleep(5)
+
+
+watchForChanges()
